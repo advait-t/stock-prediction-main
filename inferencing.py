de6542fbbf99f4a8dc84e3b731e4a831b2e77363
advait-t <60815910+advait-t@users.noreply.github.com>
2022-10-21 13:07:46 +0530

    Update inferencing.py

--- inferencing.py
+++ inferencing.py
@@ -44,7 +44,7 @@ def inferencing(holiday_list_path, training_data_path, error_df_path, model_path
 #! Loading Model
 def load_model(model_path):
 #     with open(model_path, 'r') as fin:
-    saved_model = model_from_json(json.load(fin))  # Load model
+    saved_model = model_from_json(json.load(model_path))  # Load model
     return saved_model
 
 #! check for holiday
